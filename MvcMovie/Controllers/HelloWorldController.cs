67a5fb9d2038444f1135d00ecd8edff340b3c201
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-10 10:15:50 +0100

    End of Part2: add a controller

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -13,8 +13,18 @@ public class HelloWorldController : Controller
     }
     // 
     // GET: /HelloWorld/Welcome/ 
-    public string Welcome()
+    //public string Welcome()
+    //{
+    //    return "This is the Welcome action method...";
+    //}
+
+    //public string Welcome(string name, int numTimes = 1)
+    //{
+    //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    //}
+
+    public string Welcome(string name, int ID = 1)
     {
-        return "This is the Welcome action method...";
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
